4582ca1dc563b1b4c2440d3287f93cc1ab7669cb
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-09-11 16:13:34 +0200

    Setup files

---
+++ canvas.js
@@ -0,0 +1,11 @@
+export const canvas = document.querySelector("canvas");
+export const ctx = canvas.getContext("2d");
+
+export const margin = 60;
+
+canvas.width = 1200 + 2 * margin;
+canvas.height = 600 + 2 * margin;
+
+export function clearCanvas(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+}
