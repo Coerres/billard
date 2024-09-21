e31237c2521130e91029f486b2b4ea4f2c0cd406
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-09-21 22:45:36 +0200

    Add changes

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 import { Ball } from "./Ball.js";
 import { clearCanvas } from "./canvas.js";
-import { balls } from "./setupBalls.js";
+import { Controller } from "./Controller.js";
+import { mouse } from "./mouse.js";
+import { balls, whiteBall } from "./setupBalls.js";
 import { drawCloth, drawWood } from "./table.js";
 
+const controller = new Controller(whiteBall);
 
 drawCloth();
 drawWood();
 
 balls.forEach((ball) => ball.draw());
 
-/*
 function loop(){
     clearCanvas();
     drawCloth();
     drawWood();
-    b.update();
-    b.draw();
+    balls.forEach((b) => b.update());
+    balls.forEach((b) => b.draw());
+    controller.draw();
     requestAnimationFrame(loop);
 }
 
 loop();
-*/
